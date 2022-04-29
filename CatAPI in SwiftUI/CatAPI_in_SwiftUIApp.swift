3226041c6ee0940782039854c810579d16c03795
Selin Çağlar <58820720+selincsc@users.noreply.github.com>
2022-04-29 03:10:49 +0300

    Initial Commit

---
+++ CatAPI in SwiftUI/CatAPI_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatAPI_in_SwiftUIApp.swift
+//  CatAPI in SwiftUI
+//
+//  Created by Selin Çağlar on 29.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CatAPI_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
